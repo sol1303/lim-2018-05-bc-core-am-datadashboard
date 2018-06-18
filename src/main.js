83b646161e2941e0ca89562fd78c5a3a80ec6e8b
fiorellasaro <fiorella.sfl@gmail.com>
2018-06-18 12:50:12 -0500

    Usando por primera  vez y correctamente el HTTPRequest, parse, etc

--- src/main.js
+++ src/main.js
@@ -1,34 +1,54 @@
+handleError = () => {
+    console.log( 'Ha ocurrido un error.' );
+  }
+
+getCohorts = () => {
+    const cohortsJSON = JSON.parse(cohortsRequest.responseText);
+    console.log(cohortsJSON);
+    /*cohortsJSON['0'].id*/
+
+};
 const requestURLCohorts = '../data/cohorts.json';
-const requestURLProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const requestURLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-
-
-const usersRequest = new XMLHttpRequest();
-const progressRequest = new XMLHttpRequest();
-
-
 const cohortsRequest = new XMLHttpRequest();
 cohortsRequest.open('GET', requestURLCohorts);
+cohortsRequest.onload = getCohorts;
+cohortsRequest.onerror = handleError;
 cohortsRequest.send();
 
-cohortsRequest.onload = function() {
-    const cohortsJSON = JSON.parse(this.responseText);
-    const response = cohortsJSON.response;
-    console.log(response);
+/*
+getUsers = () => {
+    const userJSON = JSON.parse(usersRequest.responseText);
+    console.log(userJSON);
 };
-
-
-
+const requestURLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const usersRequest = new XMLHttpRequest();
 usersRequest.open('GET', requestURLUsers);
-progressRequest.open('GET', requestURLProgress);
+usersRequest.onload = getUsers;
+usersRequest.onerror = handleError;
+usersRequest.send();
 
-usersRequest.onload = () => {
-   userJSON = JSON.parse(usersRequest.responseText);
+
+
+getProgress = () => {
+    const progressJSON = JSON.parse(progressRequest.responseText);
+    console.log(progressJSON);
 };
+const requestURLProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const progressRequest = new XMLHttpRequest();
+progressRequest.open('GET', requestURLProgress);
+progressRequest.onload = getProgress;
+progressRequest.onerror = handleError;
+progressRequest.send();
 
-progressRequest.onload = () => {
-    progressJSON = JSON.parse(progressRequest.responseText);
- };
+*/
+
+
+
+
+
+
+
+
 
  
  /*
