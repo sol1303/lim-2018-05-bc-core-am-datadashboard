4650e498fb28cc2833a03850c1fbd8c7d0d2efc6
fiorellasaro <fiorella.sfl@gmail.com>
2018-06-20 08:08:27 -0500

    cambios menores en actualizacion.

--- src/main.js
+++ src/main.js
@@ -1,66 +1,3 @@
-handleError = () => {
-    console.log( 'Ha ocurrido un error.' );
-  }
-
-getCohorts = () => {
-    const cohortsJSON = JSON.parse(cohortsRequest.responseText);
-    console.log(cohortsJSON);
-    /*cohortsJSON['0'].id*/
-
-};
-const requestURLCohorts = '../data/cohorts.json';
-const cohortsRequest = new XMLHttpRequest();
-cohortsRequest.open('GET', requestURLCohorts);
-cohortsRequest.onload = getCohorts;
-cohortsRequest.onerror = handleError;
-cohortsRequest.send();
-
-/*
-getUsers = () => {
-    const userJSON = JSON.parse(usersRequest.responseText);
-    console.log(userJSON);
-};
-const requestURLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const usersRequest = new XMLHttpRequest();
-usersRequest.open('GET', requestURLUsers);
-usersRequest.onload = getUsers;
-usersRequest.onerror = handleError;
-usersRequest.send();
-
-
-
-getProgress = () => {
-    const progressJSON = JSON.parse(progressRequest.responseText);
-    console.log(progressJSON);
-};
-const requestURLProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const progressRequest = new XMLHttpRequest();
-progressRequest.open('GET', requestURLProgress);
-progressRequest.onload = getProgress;
-progressRequest.onerror = handleError;
-progressRequest.send();
-
-*/
-
-
-
-
-
-
-
-
-
- 
- /*
-for (const iterator of object) {
-    
-} UTILIZAR  "forof"
- utilizar  "arrays.map"
- utilizar "arrays.filter"
- http-server . (para acceder a un servidor de node - debo entrar al shell de node)
-*/
-
- 
 let buttonEnter = () => {
     document.getElementById('main').style.display = 'none'; 
     document.getElementsByClassName('data')[0].style.display = 'initial'
@@ -157,7 +94,11 @@ let buttonEnter = () => {
   
 
 
-
+handleError = () => {
+    console.log( 'Ha ocurrido un error.' );
+}
+   
+  
 const requestURLCohorts = '../data/cohorts.json';
 const cohortsRequest = new XMLHttpRequest();
 const requestURLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
@@ -184,6 +125,7 @@ getUsers = (cohorts) => {
   const usersJSON = JSON.parse(usersRequest.responseText);
   getProgress(cohorts,usersJSON); //pasa el cohort y los usuarios del cohort
   };
+  usersRequest.onerror = handleError;
   usersRequest.send();
 };
 
@@ -205,6 +147,7 @@ getProgress = (cohorts,users) =>{
       
 
   };
+  progressRequest.onerror = handleError;
   progressRequest.send(); 
 };
 
@@ -212,4 +155,5 @@ getProgress = (cohorts,users) =>{
 
 cohortsRequest.open('GET', requestURLCohorts);
 cohortsRequest.onload = getCohorts;
+cohortsRequest.onerror = handleError;
 cohortsRequest.send();
