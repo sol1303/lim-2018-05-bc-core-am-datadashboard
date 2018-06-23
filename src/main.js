90192fb034fee9e013d4286099c65b8abe668e7e
fiorellasaro <fiorella.sfl@gmail.com>
2018-06-23 17:57:37 -0500

    Uso de jquery ajax, adicion de libreria jquery en html, la lista de estudiantes y filtrado solo se ve en consola por el momento.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 let buttonEnter = (users) => {
-
-  console.log(users);
   document.getElementById('main').style.display = 'none'; 
   document.getElementsByClassName('data')[0].style.display = 'initial';
- for (const user of users){
+  /*for (const user of users){
     let std = {
       name : users.name,
       percent : 100,
@@ -26,16 +24,9 @@ let buttonEnter = (users) => {
       },  
     }
     createCard(std);
-  }
+  } */
+ 
 };
-  
-
-/*let removeOptions = () => {
-  document.querySelector('.sedeCohorts').removeChild('cohorts');
-} */
-
-
-
 
 let createCard = (student) => {
   let divTab = document.createElement('DIV');
@@ -94,20 +85,145 @@ const createCardRow = (key, value) => {
   
     return row; 
   };
-
-
-
   
+handleError = (xhr,status,error) => {
+    console.log( 'Ha ocurrido un error.' );
+};
 
-const init = (callback) => {
-  
-  handleError = () => {
-      console.log( 'Ha ocurrido un error.' );
-  };
-     
+$.ajax({
+  type: "GET",
+  url:'../data/cohorts.json',
+  success: function passingCohort (data){
+    let cohorts = data;
+    let cohort;
+    let listOptions = (id) => {
+      let cohortOption = document.createElement('OPTION');
+      cohortOption.setAttribute('class', 'sedeCohorts')
+      let txtCohort = document.createTextNode(id);
+      cohortOption.appendChild(txtCohort);
+      document.getElementById('cohorts').appendChild(cohortOption);
     
-  const requestURLCohorts = '../data/cohorts.json';
-  const cohortsRequest = new XMLHttpRequest();
+      return cohortOption;
+    };
+    let selectSede = () => {
+      for (const cohort of cohorts ){
+      if (document.getElementById('sede').value === 'LIM') {
+          if(cohort.id.includes('lim-')){
+            listOptions(cohort.id);
+          }
+      } 
+      if (document.getElementById('sede').value === 'AQP'){
+        if(cohort.id.includes('aqp-')){
+          listOptions(cohort.id);
+        }
+      } else if (document.getElementById('sede').value === 'CDMX'){
+        if(cohort.id.includes('cdmx-')){
+          listOptions(cohort.id);
+        }
+      }
+      else if (document.getElementById('sede').value === 'GUADALAJARA') {
+        if(cohort.id.includes('gdl-')){
+          listOptions(cohort.id);
+        }
+      }
+      else if (document.getElementById('sede').value === 'SANTIAGO'){
+        if(cohort.id.includes('scl-')){
+          listOptions(cohorts.id);
+        }
+      }
+    }}; 
+
+    for (const cohort of cohorts ){
+          if(cohort.id.includes('lim-')){
+            listOptions(cohort.id);
+          }
+      };
+
+    document.getElementById('sede').addEventListener('change', selectSede);
+    document.getElementById('enter').addEventListener('click', buttonEnter);
+    document.getElementById('cohorts').addEventListener('change', selectCohort = () => {
+      let cohortSelected= document.querySelector('#cohorts').value;
+        for (const cohor of cohorts){
+          if (cohor.id == cohortSelected){
+            cohort= cohor;
+            getUsers(cohort);
+          }
+        }
+      });
+  },
+  error: handleError
+});
+
+function getUsers(cohort)  {
+      $.ajax({
+        type: "GET",
+        url:'../data/cohorts/lim-2018-03-pre-core-pw/users.json',
+        success: function passingUsers (data){
+          let users = data;
+          const usersCohort = users.filter(userFilter => userFilter.signupCohort === cohort.id); //filtrado usuarios por cohort
+          getProcess(cohort, usersCohort);
+        }, 
+        error: handleError
+      });
+          
+};
+
+function getProcess (cohort, usersCohort) {
+    
+    $.ajax({
+      type: "GET",
+      url:'../data/cohorts/lim-2018-03-pre-core-pw/progress.json',
+      success: function(data){
+        let progress = data;
+        console.log(cohort);
+        console.log(usersCohort);
+        console.log(progress);
+        const options = { //este objeto es el que piden para la funcion que engloba las 3 funciones en el data.js
+          cohort: cohort,
+          cohortData: {
+          users: usersCohort, 
+          progress: progress
+          },
+          orderBy: '', //String con criterio de ordenado (ver sortUsers).
+          orderDirection: '', //String con dirección de ordenado (ver sortUsers).
+          search: '' //String de búsqueda (ver filterUsers)
+       };
+        datadashboard.processCohortData(options); //aqui se utiliza el objeto options
+        for (const user of usersCohort){
+          let std = {
+            name : user.name,
+            percent : 100,
+            exercise : {
+              total : 25,
+              completed : 10,
+              percent : 30,
+            } ,
+            reads : {
+              total : 45,
+              completed : 15,
+              percent : 60,
+            } ,
+            quizzes : {
+              total : 25,
+              completed : 15,
+              percent : 30,
+              scoreSum : 60,
+              scoreAvg : 30,
+            },  
+          }
+          console.log(std);
+        }
+      },
+      error: handleError
+    });
+};
+
+
+
+
+/*
+const requestURLCohorts = '../data/cohorts.json';
+const cohortsRequest = new XMLHttpRequest();
   const requestURLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
   const usersRequest = new XMLHttpRequest();
   const requestURLProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
@@ -154,28 +270,22 @@ const init = (callback) => {
       }
       i++;
     }};
+
     for (const cohort of cohortsJSON ){
           if(cohortsJSON[i].id.includes('lim-')){
             listOptions(cohortsJSON[i].id);
           }
       i++;
       };
-  
-   /* let idCohort=0;
+   let idvalue=0;
    let idValue = () =>{
-     if(idCohort==0){
-      idCohort++;
-      getUsers(cohortsJSON);
-     }else{
-      getUsers(cohortsJSON); //se pasa el arreglo del cohort en cuestion
-     }
-     
+    idvalue ++;
+    console.log(idvalue);
    } 
-    document.getElementById('cohorts').addEventListener('change', idValue);*/
+   document.getElementById('cohorts').addEventListener('change', idValue);
+    console.log(document.querySelector('#cohorts').value);
     getUsers(cohortsJSON);
-    document.getElementById('sede').addEventListener('change', selectSede);
-    
-    
+    document.getElementById('sede').addEventListener('change', selectSede);  
   };
   
   getUsers = (cohorts) => {
@@ -199,6 +309,7 @@ const init = (callback) => {
       let co;
       for (const cohort of cohorts){
         if (cohort.id ==document.querySelector('#cohorts').value){
+          console.log(document.querySelector('#cohorts').value);
           co = cohort;
         }
       }
@@ -217,9 +328,6 @@ const init = (callback) => {
       search: '' //String de búsqueda (ver filterUsers)
    };
   datadashboard.processCohortData(options); //aqui se utiliza el objeto options
-  callback(usersCohort);
-  
-  
   
   };
   
@@ -227,17 +335,14 @@ const init = (callback) => {
     progressRequest.send(); 
      
     document.getElementById('myInput').addEventListener('keyup',  filterUsers);
+     
   };
   
   
-  cohortsRequest.open('GET', requestURLCohorts);
-  cohortsRequest.onload = getCohorts;
-  cohortsRequest.onerror = handleError;
-  cohortsRequest.send();
   
+  */
     
-}
 
-init((usersCohort) => {
-  document.getElementById('enter').addEventListener('click', buttonEnter); 
-});
+
+
+  
